fa0a9326267e2a671ec2e63b2235d15be6198d3f
izumin5210 <masayuki@izumin.info>
2014-04-08 02:29:50 +0900

    Improve DepartmentsCtrl

--- javascripts/controllers/departments_ctrl.js
+++ javascripts/controllers/departments_ctrl.js
@@ -4,24 +4,23 @@
   app = angular.module('ETimetable');
 
   app.controller('DepartmentsCtrl', function($scope, $routeParams) {
-    var getHref, grade;
-    grade = $routeParams.grade;
-    getHref = function(grade, department) {
-      return "#/" + grade + "/" + department + "/" + (grade > 3 && department === 'e' ? 'courses' : 'timetables');
+    var getHref;
+    getHref = function(department) {
+      return "#/" + $routeParams.grade + "/" + department + "/" + ($routeParams.grade > 3 && department === 'e' ? 'courses' : 'timetables');
     };
     return $scope.departments = [
       {
         label: '機械工学科',
-        href: getHref(grade, 'm')
+        href: getHref('m')
       }, {
         label: '電気情報工学科',
-        href: getHref(grade, 'e')
+        href: getHref('e')
       }, {
         label: '都市システム工学科',
-        href: getHref(grade, 'c')
+        href: getHref('c')
       }, {
         label: '建築学科',
-        href: getHref(grade, 'a')
+        href: getHref('a')
       }
     ];
   });
--- src/coffeescripts/controllers/departments_ctrl.coffee
+++ src/coffeescripts/controllers/departments_ctrl.coffee
@@ -3,15 +3,13 @@ app = angular.module 'ETimetable'
 app.controller 'DepartmentsCtrl',
   ($scope, $routeParams) ->
 
-    grade = $routeParams.grade
-
-    getHref = (grade, department) ->
-      "#/#{grade}/#{department}/#{if grade > 3 && department == 'e' then 'courses' else 'timetables'}"
+    getHref = (department) ->
+      "#/#{$routeParams.grade}/#{department}/#{if $routeParams.grade > 3 && department == 'e' then 'courses' else 'timetables'}"
 
     $scope.departments = [
-      {label: '機械工学科', href: getHref(grade, 'm')}
-      {label: '電気情報工学科', href: getHref(grade, 'e')}
-      {label: '都市システム工学科', href: getHref(grade, 'c')}
-      {label: '建築学科', href: getHref(grade, 'a')}
+      {label: '機械工学科', href: getHref('m')}
+      {label: '電気情報工学科', href: getHref('e')}
+      {label: '都市システム工学科', href: getHref('c')}
+      {label: '建築学科', href: getHref('a')}
     ]
 
