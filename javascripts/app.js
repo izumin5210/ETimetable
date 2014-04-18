5512e28008931e7873bcce1de2e12fd375b0a48b
izumin5210 <masayuki@izumin.info>
2014-04-18 18:44:01 +0900

    Fix routing

--- javascripts/app.js
+++ javascripts/app.js
@@ -11,28 +11,28 @@ app.constant('config', {
 
 app.config(function($routeProvider, $locationProvider) {
   return $routeProvider.when('/grades', {
-    templateUrl: '/templates/grades.html',
+    templateUrl: './templates/grades.html',
     controller: 'GradesCtrl'
   }).when('/:grade/departments', {
-    templateUrl: '/templates/departments.html',
+    templateUrl: './templates/departments.html',
     controller: 'DepartmentsCtrl'
   }).when('/:grade/:department/courses', {
-    templateUrl: '/templates/courses.html',
+    templateUrl: './templates/courses.html',
     controller: 'CoursesCtrl'
   }).when('/:grade/:department/timetables', {
-    templateUrl: '/templates/timetables.html',
+    templateUrl: './templates/timetables.html',
     controller: 'TimetablesCtrl'
   }).when('/:grade/:department/:course/timetables', {
-    templateUrl: '/templates/timetables.html',
+    templateUrl: './templates/timetables.html',
     controller: 'TimetablesCtrl'
   }).when('/:grade/:department/timetables/:wday', {
-    templateUrl: '/templates/timetables.html',
+    templateUrl: './templates/timetables.html',
     controller: 'TimetablesCtrl'
   }).when('/:grade/:department/:course/timetables/:wday', {
-    templateUrl: '/templates/timetables.html',
+    templateUrl: './templates/timetables.html',
     controller: 'TimetablesCtrl'
   }).when('/lectures/:id', {
-    templateUrl: '/templates/lectures.html',
+    templateUrl: './templates/lectures.html',
     controller: 'LecturesCtrl'
   }).otherwise({
     redirectTo: '/grades'
